2ee5e2798b0fe88085a00d1a8103fe547eb9c11c
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-30 10:27:50 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,170 @@
+// Marvish Chandra
+
+namespace inspirationalquote{
+    class quoteTheme{
+        public static void mostFamous(string[] author){
+            if (author == "Helen Keller"){
+                Console.WriteLine("The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.");
+            }
+            if (author == "Oscar Wilde"){
+                Console.WriteLine("Keep love in your heart. A life without it is like a sunless garden when the flowers are dead.");
+            }
+            if (author == "Aristotle Onassis"){
+                Console.WriteLine("It is during our darkest moments that we must focus to see the light.");
+            }
+            if (author == "Maya Angelou"){
+                Console.WriteLine("Try to be a rainbow in someone's cloud.");
+            }
+            if (author == "Joseph Campbell"){
+                Console.WriteLine("Find a place inside where there's joy, and the joy will burn out the pain.");
+            }
+            if (author == "Audrey Hepburn"){
+                 Console.WriteLine("Nothing is impossible, the word itself says 'I'm possible'!");
+            }
+            if (author == "Robert Louis Stevenson"){
+                Console.WriteLine("Don't judge each day by the harvest you reap but by the seeds that you plant.");
+            }
+            if (author == "Buddha"){
+                Console.WriteLine("Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.");
+            }
+            if (author == "Edmund Burke"){
+                Console.WriteLine("The only thing necessary for the triumph of evil is for good men to do nothing.");
+            }
+            if (author == "Lucius Annaeus Seneca"){
+                Console.WriteLine("One of the most beautiful qualities of true friendship is to understand and to be understood.");
+            }
+
+        }
+        public static void mostInspirational(string[] author){
+            if (author == "John F. Kennedy"){
+                Console.WriteLine("If not us, who? If not now, when?");
+            }
+            if (author == "Jack Canfield"){
+                Console.WriteLine("Don’t worry about failures, worry about the chances you miss when you don’t even try.");
+            }
+            if (author == "Johann Wolfgang Von Goethe"){
+                Console.WriteLine("Live each day as if your life had just begun.");
+            }
+            if (author == "Lao Tzu"){
+                Console.WriteLine("The journey of a thousand miles begins with one step.");
+            }
+            if (author == "Thomas Carlyle"){
+                Console.WriteLine("Every noble work is at first impossible.");
+            }
+            if (author == "George Eliot"){
+                Console.WriteLine("It is never too late to be what you might have been.");
+            }
+            if (author == "John Wooden"){
+                Console.WriteLine("Do not let what you cannot do interfere with what you can do.");
+            }
+            if (author == "Babe Ruth"){
+                Console.WriteLine("Every strike brings me closer to the next home run.");
+            }
+            if (author == "Prescott"){
+                Console.WriteLine("An obstacle is often a stepping stone.");
+            }
+            if (author == "Theodore Roosevelt"){
+                Console.WriteLine("Believe you can and you’re halfway there.");
+            }
+
+        }
+        public static void aboutDeath(string[] author){
+            if (author == "Rumi"){
+                Console.WriteLine("Goodbyes are only for those who love with their eyes. Because for those who love with heart and soul there is no such thing as separation.");
+            }
+            if (author == "Maya Angelou"){
+                Console.WriteLine("I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.");
+            }
+            if (author == "Norman Cousins"){
+                Console.WriteLine("Death is not the greatest loss in life. The greatest loss is what dies inside us while we live.");
+            }
+            if (author == "Marcus Aurelius"){
+                Console.WriteLine("Death smiles at us all, all a man can do is smile back.");
+            }
+            if (author == "Albert Einstein"){
+                Console.WriteLine("Our death is not an end if we can live on in our children and the younger generation. For they are us; our bodies are only wilted leaves on the tree of life.");
+            }
+            if (author == "Washington Irving"){
+                Console.WriteLine("There is a sacredness in tears. They are not the mark of weakness, but of power. They speak more eloquently than ten thousand tongues. They are the messengers of overwhelming grief, of deep contrition, and of unspeakable love.");
+            }
+            if (author == "Rabindranath Tagore"){
+                Console.WriteLine("Death is not extinguishing the light; it is only putting out the lamp because the dawn has come.");
+            }
+            if (author == "Patrick Swayze"){
+                Console.WriteLine("When those you love die, the best you can do is honor their spirit for as long as you live. You make a commitment that you're going to take whatever lesson that person or animal was trying to teach you, and you make it true in your own life... It's a positive way to keep their spirit alive in the world, by keeping it alive in yourself.");
+            }
+            if (author == "Oscar Wilde"){
+                Console.WriteLine("Death must be so beautiful. To lie in the soft brown earth, with the grasses waving above one's head, and listen to silence. To have no yesterday, and no tomorrow. To forget time, to forgive life, to be at peace.");
+            }
+            if (author == "Joan Baez"){
+                Console.WriteLine("You don't get to choose how you're going to die, or when. You can only decide how you're going to live. Now.");
+            }
+
+    }
+    public static void aboutLove(string[] author){
+        if (author == "Gaby Dunn){
+            Console.WriteLine("And now we’re apart and you’re just some stranger who knows all my secrets and all my family members and all my quirks and flaws and it doesn’t make sense.");
+        }
+        if (author == "Sarah Dessen"){
+            Console.WriteLine("There is never a time or place for true love. It happens accidentally, in a heartbeat, in a single flashing, throbbing moment.");
+        }
+        if (author == "Mark Twain"){
+            Console.WriteLine("Love is the irresistible desire to be irresistibly desired.");
+        }
+        if (author == "Ralph Waldo Emerson"){
+            Console.WriteLine("Thou art to me a delicious torment.");
+        }
+        if (author == "Mother Teresa"){
+            Console.WriteLine("If you judge people, you have no time to love them.");
+        }
+        if (author == "Robert A. Heinlein"){
+            Console.WriteLine("Love is that condition in which the happiness of another person is essential to your own.");
+        }
+        if (author == "Orson Welles"){
+            Console.WriteLine("We’re born alone, we live alone, we die alone. Only through our love and friendship can we create the illusion for the moment that we’re not alone.");
+        }
+        if (author == "Clarice Lispector"){
+            Console.WriteLine("Love is now, is always. All that is missing is the coup de grace -- which is called passion.");
+        }
+        if (author == "Aristotle"){
+            Console.WriteLine("Love is composed of a single soul inhabiting two bodies.");
+        }
+        if (author == "Roy Croft"){
+            Console.WriteLine("I love you, not only for what you are but for what I am when I am with you.");
+        }
+
+    }
+    public static void aboutLife(string[] author){
+        if (author == "J.M. Barrie"){
+            Console.WriteLine("Not in doing what you like, but in liking what you do is the secret of happiness");
+        }
+        if (author == "George Clooney"){
+            Console.WriteLine("My father gave me the best advice of my life. He said, ‘Whatever you do, don’t wake up at 65 years old and think about what you should have done with your life.");
+        }
+        if (author == "Katherine Mansfield"){
+            Console.WriteLine("Make it a rule of life never to regret and never to look back. Regret is an appalling waste of energy; you can’t build on it; it’s only good for wallowing.")
+        }
+        if (author == "Simone Biles"){
+            Console.WriteLine("I’d rather regret the risks that didn’t work out than the chances I didn’t take at all.")
+        }
+        if (author == "Jonathan Winters"){
+            Console.WriteLine("If your ship doesn’t come in, swim out to it! ");
+        }
+        if (author == "Robert Mansell"){
+            Console.WriteLine("A pessimist is one who makes difficulties out of his opportunities; an optimist is one who makes opportunities out of his difficulties.");
+        }
+        if (author == "Archbishop Desmond Tutu"){
+            Console.WriteLine("My father used to say, ‘Don’t raise your voice. Improve your argument.'");
+        }
+        if (author == "H.G. Wells"){
+            Console.WriteLine("What on earth would a man do with himself if something did not stand in his way?");
+        }
+        if (author == "Columbia Record"){
+            Console.WriteLine("‘Don’t worry’ makes a better motto when you add ‘others.'");
+        }
+        if (author == "Jon Stewart"){
+            Console.WriteLine("When you are actually powerful, you don’t need to be petty.");
+        }
+
+    }
+    }
